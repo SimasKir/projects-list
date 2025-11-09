8a19381d164714a95e37634abb83689b42f89644
SimasKiriliauskas <simas.kiriliauskas@theimpactbrands.com>
2025-11-09 19:33:14 +0200

    feat: add heading

--- src/components/projects-list/projects-list.tsx
+++ src/components/projects-list/projects-list.tsx
@@ -13,6 +13,12 @@ export const ProjectsList = () => {
 
   return (
     <div className="flex flex-col gap-3 container py-20 pb-30">
+      <div className="grid mb-5 w-full [grid-template-columns:repeat(2,minmax(0,1fr))]">
+        <h1 className="text-[38px] leading-[42px] font-bold col-span-1">
+          Investavimo galimybės užtikrintos nekilnojamuoju turtu
+        </h1>
+        <div className="col-span-1"></div>
+      </div>
       <ProjectsHeader />
       {projects.map((project) => (
         <ProjectCard project={project} key={project.pid} />
